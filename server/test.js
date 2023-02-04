af926ec4f1f51b163cb8a25c9e92f05679cb16c7
EndyBrownNB <endybrown1987@gmail.com>
2023-02-04 17:35:19 +0300

    03 Try Get Dev

--- server/test.js
+++ server/test.js
@@ -1,10 +1,32 @@
+const express = require('express');
+const { response } = require("express");
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 function randomNum(max){
     const num = Math.floor(Math.random()*max)
-    console.log(num)
-    setTimeout(randomNum, 2000, num+200)
-    //delay(randomNum(num+200), 2000)
+    return num
+    //setTimeout(randomNum, 2000, num+100)
 }
 
-randomNum(500)
+randomNum(100)
+
+const app = express()
+app.use(bodyParser.json())
+app.use(cors())
+const port = 3080
+const host = '192.168.1.83'
+
+app.get('/test', async (req,res) => {
+    const response = await randomNum(100);
+    console.log(response.data)
+    res.json({
+        models: response.data.data
+    })
+});
+
+
+app.listen(port, host, () =>{
+    console.log(`Example app listening at http://${host}:${port}`)
+})
