150cff95721145fdf7bd63e7fcd0e42d416376b1
EndyBrownNB <endybrown1987@gmail.com>
2023-02-05 19:24:08 +0300

    05 sync_fetch_is_working_Dev

---
+++ server/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const { response } = require("express");
+const path = require('path')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const ejs = require('ejs');
+const si = require('systeminformation')
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+
+const port = 3000;
+const host = '192.168.1.83'
+
+var num = 0
+
+function randomNum(){
+    num = num+1
+    console.log(num)
+    setTimeout(randomNum, 1000)
+}
+
+randomNum();
+
+const app = express()
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/num', (req, res) => {
+    res.json({ msg: num });
+  });
+
+
+  app.listen(port, host, () =>{
+    console.log(`numServer listening at http://${host}:${port}`)
+})
--- server/test.js
+++ server/test.js
@@ -1,41 +1,45 @@
 const express = require('express');
 const { response } = require("express");
+const path = require('path')
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const ejs = require('ejs');
+const si = require('systeminformation')
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 
-// var a = 0
-// var b = 0
-
-// function ab(){
-//     a = 4
-//     b = 3
-// }
-// ab()
-// console.log(`a:${a} b:${b}`)
 var num = 0
+
 function randomNum(){
-    //const num = Math.floor(Math.random()*max)
     num = num+1
     console.log(num)
     setTimeout(randomNum, 1000)
 }
 
-//setTimeout(randomNum, 1000)
-randomNum();
-// console.log(num)
+//randomNum();
 
 const app = express()
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.json())
 app.use(cors())
 const port = 3080
 const host = '192.168.1.83'
 
-app.get('/test', async (req,res) => {
-    const response = num;
-    console.log(`response: ${response}`)
-    res.json({
-        models: response
+// function evrsec(res,response){
+// return res.json({
+//     models: response
+// })
+// }
+
+// app.get('/test', async (req,res) => {
+//     const response = num;
+//     console.log(`response: ${response}`)
+//     setTimeout(evrsec, 1000,res,response)
+// });
+
+app.get('/data', (req, res) => {
+    res.render('example', {
+        msg: 'start'
     })
 });
 
