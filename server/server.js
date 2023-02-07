06d885b24695fce8aa89488f5d164044aa3f23cc
EndyBrownNB <endybrown1987@gmail.com>
2023-02-07 17:34:27 +0300

    08 let's_canvas_Dev

--- server/server.js
+++ server/server.js
@@ -6,35 +6,27 @@ const cors = require('cors');
 const ejs = require('ejs');
 const si = require('systeminformation')
 const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
-const oke = require('./si');
+const findProcces = require('./si');
 const { error } = require('console');
 
 
 const port = 3000;
 const host = '192.168.1.83'
 
-//var num = interval()//.then(data => {return data})
 async function newInterval(){
   return require('./si')
 } 
 
-
-function randomNum(){
-    num = num+1
-    console.log(num)
-    setTimeout(randomNum, 1000)
-}
-
-//randomNum();
 
 const app = express()
 app.use(bodyParser.json())
 app.use(cors())
 
 app.get('/num', (req, res) => {
-  oke().then(data => {
-      console.log(data.cpuu)
-      res.json({msg: data.cpuu});
+  console.log(`REQ: ${req.url} ======`)
+  findProcces('Heroes3 HD.exe').then(data => {
+      console.log(`DATA: ${data.cpu}`)
+      res.json({msg: `CPU: ${data.cpu}`});
   }).catch(error => {
     console.error('There is a problem in res num');
     res.status(500).send({ error:'there is error'});
--- server/si.js
+++ server/si.js
@@ -9,76 +9,34 @@ async function findProcces(procName){
           for(let i=0;i<data.list.length;i++){
              if(data.list[`${i}`].name == procName){
                  return res = {
-                //     name: data.list[i].name,
                      cpus: data.list[i].pcpus,
                      cpu: data.list[i].pcpu,
                      cpuu: data.list[i].pcpuu,
                      pmem: data.list[i].pmem
                  }
-                //console.log(`${i}`)
-                //console.log(`${Object.keys(data.list[i])}`)
-                //console.log(`pid: ${data.list[i].pid}`)
-                //console.log(`pcpu: ${data.list[i].pcpu}`)
-                //console.log(`pcpuu: ${data.list[i].pcpuu}`)
-                //console.log(`pcpus: ${data.list[i].pcpus}`)
-                //console.log(`pmem: ${data.list[i].pmem}`)
-                //console.log(`mem_vsz: ${data.list[i].mem_vsz}`)
-                //console.log(`mem_rss: ${data.list[i].mem_rss}`)
-                //console.log(`pid: ${data.list[i].parentPid}`)
+
                 }
             }
         })
         return res
 }
 
-//findProcces('Heroes3 HD.exe')
 
 function interval2 (){
-    return findProcces('Heroes3 HD.exe').then((res) => {console.log(`_____\nCPUU: ${res.cpuu}\nCPUS: ${res.cpus}\nCPU: ${res.cpu}\nMEM: ${res.pmem}`)})
-}
-
-setInterval(interval2,2000)
-
-//module.exports = oke;
-
-
-async function order(){
-    await si.processes()
-    .then(data => {
-        console.log(`order: ${data.list[`230`].name}`)
-        return data.list[`230`].name
-                
-        })
-        .catch(error => console.error(`${error}`));
-        
-}
-
-async function oke(){
     let res;
-    await si.processes()
-        .then(data => {
-            res =  {
-                cpuu:data.list['247'].cpuu,
-                name:data.list['247'].name
-            }   
-            return res
-    })
-    .catch(error => console.error(`${error}`));
+    return findProcces('Heroes3 HD.exe')
+        .then((data) => {return res = {cpus: cpus,
+            cpu: data.list[i].pcpu,
+            cpuu: data.list[i].pcpuu,
+            pmem: data.list[i].pmem
+        }})
     return res
+    //{console.log(`_____\nCPUU: ${res.cpuu}\nCPUS: ${res.cpus}\nCPU: ${res.cpu}\nMEM: ${res.pmem}`)})
 }
 
-// const processCpuu = si.processes()
-//     .then(data => {
-//         res =  {
-//             cpuu:data.list['178'].cpuu,
-//         }   
-//         return res
-//     })
 
+module.exports = findProcces;
 
-function interval (){
-    return processCpuu.then(val => {console.log(val.cpuu)})
-}
 
 
 
